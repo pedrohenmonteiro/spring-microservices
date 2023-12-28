7199554d782def496c33551d4e58a27df0772b0b
Pedro Monteiro <pedromont436@gmail.com>
2023-12-28 00:30:52 -0300

    criando producer

---
+++ user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,34 @@
+package com.ms.user.producers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.ms.user.dtos.EmailDto;
+import com.ms.user.models.User;
+
+@Component
+public class UserProducer {
+    
+    final RabbitTemplate rabbitTemplate;
+
+    
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(User user) {
+        var emailDto = new EmailDto();
+        emailDto.setUserId(user.getUserId());
+        emailDto.setEmailTo(user.getEmail());
+        emailDto.setSubject("Cadastro realizado com sucesso!");
+        emailDto.setText(user.getName() + ", seja bem vindo(a)!" );
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+
+}
--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ms.user.models.User;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,9 +15,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserProducer userProducer;
+
     @Transactional
     public User save(User user) {
-        return userRepository.save(user);
+       user = userRepository.save(user);
+
+       userProducer.publishMessageEmail(user);
+
+        return user;
     }
 
 }
